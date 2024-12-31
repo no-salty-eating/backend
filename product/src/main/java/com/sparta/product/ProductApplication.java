1af139eca0120862d791418d1f6e495d82ca8e89
HongChan Yoon <hongchani0324@gmail.com>
2024-12-31 14:33:03 +0900

    [feature] #2: product init commit - port adaptor 패턴 적용 - QueryDslConfig.java 설정 - KafkaConfig.java 설정

--- product/src/main/java/com/sparta/product/ProductApplication.java
+++ product/src/main/java/com/sparta/product/ProductApplication.java
@@ -2,8 +2,17 @@ package com.sparta.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableScheduling
+//@EnableFeignClients
+@EnableKafka
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class ProductApplication {
 
 	public static void main(String[] args) {
