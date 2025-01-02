d231b88e7a2df0038f8d87de60cc166d08cf531b
32163555 <dlwnstjr0310@gmail.com>
2025-01-02 16:55:10 +0900

    [#1] feat : order setup

---
+++ order/src/main/kotlin/com/study/order/OrderApplication.kt
@@ -0,0 +1,13 @@
+package com.study.order
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
+
+@EnableR2dbcAuditing
+@SpringBootApplication
+class OrderApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrderApplication>(*args)
+}
---
+++ order/src/main/kotlin/com/study/order/presentation/api/controller/docs/OrderControllerDocs.kt
@@ -0,0 +1,7 @@
+package com.study.order.presentation.api.controller.docs
+
+import io.swagger.v3.oas.annotations.tags.Tag
+
+@Tag(name = "Order" , description = "주문 API")
+open class OrderControllerDocs {
+}
