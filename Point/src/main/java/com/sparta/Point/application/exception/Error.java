f4208dfd87f9760bec270bff8730c3715d80301f
yanjuicy <yanju@yanjuui-MacBookPro.local>
2025-01-06 10:40:50 +0900

    [feature] #5: Point init commit

---
+++ Point/src/main/java/com/sparta/Point/application/exception/Error.java
@@ -0,0 +1,19 @@
+package com.sparta.Point.application.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@AllArgsConstructor
+@FieldDefaults(makeFinal = true)
+public enum Error {
+
+    INTERNAL_SERVER_ERROR(9999, "서버 오류입니다."),
+    CIRCUIT_BREAKER_OPEN(10000, "이용량 증가로 현재 서비스가 불가능합니다."),
+    SERVER_TIMEOUT(10001, "응답 시간을 초과하였습니다.");
+
+    Integer code;
+    String message;
+
+}
---
+++ Point/src/main/java/com/sparta/Point/application/exception/PointException.java
@@ -0,0 +1,16 @@
+package com.sparta.Point.application.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class PointException extends RuntimeException {
+
+    private final Error error;
+    private final HttpStatus httpStatus;
+
+    public PointException(Error error, HttpStatus httpStatus) {
+        this.error = error;
+        this.httpStatus = httpStatus;
+    }
+}
