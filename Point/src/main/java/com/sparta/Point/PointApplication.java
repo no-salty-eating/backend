1ad6d574e1aae4c516c1003e3b22b130df94d4c7
yanjuicy <yanju@yanjuui-MacBookPro.local>
2025-01-06 15:14:59 +0900

    [feature] #5: Point init commit

--- Point/src/main/java/com/sparta/Point/PointApplication.java
+++ Point/src/main/java/com/sparta/Point/PointApplication.java
@@ -2,8 +2,10 @@ package com.sparta.Point;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PointApplication {
 
 	public static void main(String[] args) {
