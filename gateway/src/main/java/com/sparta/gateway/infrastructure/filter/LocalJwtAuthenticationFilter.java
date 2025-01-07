0b21df864c39ed3015112864b4a89fa63fdb2dbb
lje-ejl <zizz135777@gmail.com>
2025-01-08 02:35:33 +0900

    refactor : #32 리뷰 기반으로 gateway 리팩토링

--- gateway/src/main/java/com/sparta/gateway/infrastructure/filter/LocalJwtAuthenticationFilter.java
+++ gateway/src/main/java/com/sparta/gateway/infrastructure/filter/LocalJwtAuthenticationFilter.java
@@ -1,24 +1,32 @@
 package com.sparta.gateway.infrastructure.filter;
 
 
+import static com.sparta.gateway.application.exception.Error.FORBIDDEN;
+
+import com.sparta.gateway.application.exception.GatewayException;
 import com.sparta.gateway.infrastructure.util.JwtUtil;
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Slf4j(topic = "filter")
 @Component
 @RequiredArgsConstructor
 public class LocalJwtAuthenticationFilter implements GlobalFilter {
 
     private final JwtUtil jwtUtil;
+
+    // 사용자 헤더 id 변수
+    public static final String  LOGIN_ID = "X-UserId";
+
+    // 사용자 헤더 role 변수
+    public static final String  ROLE = "X-Role";
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
@@ -27,12 +35,12 @@ public class LocalJwtAuthenticationFilter implements GlobalFilter {
         // 헤더 초기화: 검증 전 기본값 설정
         exchange = exchange.mutate()
                 .request(exchange.getRequest().mutate()
-                        .header("X-UserId", "null")
-                        .header("X-Role", "null")
+                        .header(LOGIN_ID, "null")
+                        .header(ROLE, "null")
                         .build())
                 .build();
 
-        if (path.equals("/auth/logIn") || path.equals("/auth/signIn")) {
+        if (path.equals("/auth/login") || path.equals("/auth/join")) {
             return chain.filter(exchange);
         }
 
@@ -40,8 +48,7 @@ public class LocalJwtAuthenticationFilter implements GlobalFilter {
 
         if (token == null || !jwtUtil.validateToken(token)) {
             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-            String errorMessage = "Unauthorized access: Invalid or missing token.";
-            return writeErrorResponse(exchange, errorMessage);
+            throw new GatewayException(FORBIDDEN, HttpStatus.UNAUTHORIZED);
         }
 
         // 토큰 검증 성공 시 헤더 덮어쓰기
@@ -49,22 +56,13 @@ public class LocalJwtAuthenticationFilter implements GlobalFilter {
         // 새 요청 객체를 만들어서 헤더를 설정하고, 기존 exchange의 요청으로 교체
         exchange = exchange.mutate()
                 .request(exchange.getRequest().mutate()
-                        .header("X-UserId", claims.getSubject())
-                        .header("X-Role", claims.get("role", String.class))
+                        .header(LOGIN_ID, claims.getSubject())
+                        .header(ROLE, claims.get("role", String.class))
                         .build())
                 .build();
 
         return chain.filter(exchange);
     }
 
-    // 401 에러 응답 본문에 오류 메시지를 작성하는 메서드
-    private Mono<Void> writeErrorResponse(ServerWebExchange exchange, String message) {
-        // 응답 본문을 설정
-        exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
-        String responseBody = String.format("{\"error\": \"%s\"}", message);
-        DataBuffer buffer = new DefaultDataBufferFactory().wrap(responseBody.getBytes());
-
-        return exchange.getResponse().writeWith(Mono.just(buffer));
-    }
 
 }
--- gateway/src/main/java/com/sparta/gateway/infrastructure/util/JwtUtil.java
+++ gateway/src/main/java/com/sparta/gateway/infrastructure/util/JwtUtil.java
@@ -1,6 +1,13 @@
 package com.sparta.gateway.infrastructure.util;
 
 
+import static com.sparta.gateway.application.exception.Error.EXPIRED_JWT_TOKEN;
+import static com.sparta.gateway.application.exception.Error.INVALID_JWT_TOKEN;
+import static com.sparta.gateway.application.exception.Error.INVALID_TOKEN_SIGNATURE;
+import static com.sparta.gateway.application.exception.Error.NOT_FOUND_TOKEN;
+import static com.sparta.gateway.application.exception.Error.UNSUPPORTED_JWT_TOKEN;
+
+import com.sparta.gateway.application.exception.GatewayException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
@@ -13,6 +20,7 @@ import java.security.Key;
 import java.util.Base64;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
@@ -20,6 +28,13 @@ import org.springframework.web.server.ServerWebExchange;
 @Slf4j(topic = "JwtUtil")
 @Component
 public class JwtUtil {
+
+    // Header KEY 값
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    // Token 식별자
+    public static final String BEARER_PREFIX = "Bearer ";
+
     @Value("${service.jwt.secret-key}") // Base64 Encode 한 SecretKey
     private String secretKey;
     private Key key;
@@ -31,11 +46,11 @@ public class JwtUtil {
     }
 
     public String extractToken(ServerWebExchange exchange) {
-        String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
-        if (StringUtils.hasText(authHeader) && authHeader.startsWith("Bearer ")) {
+        String authHeader = exchange.getRequest().getHeaders().getFirst(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(authHeader) && authHeader.startsWith(BEARER_PREFIX)) {
             return authHeader.substring(7);
         }
-        return null;
+        throw new GatewayException(NOT_FOUND_TOKEN, HttpStatus.NOT_FOUND);
     }
 
     public Claims parseClaims(String token) {
@@ -51,14 +66,17 @@ public class JwtUtil {
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
             return true;
         } catch (SecurityException | MalformedJwtException | SignatureException e) {
-            log.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
+            // 잘못된 JWT 서명
+            throw new GatewayException(INVALID_TOKEN_SIGNATURE, HttpStatus.UNAUTHORIZED);
         } catch (ExpiredJwtException e) {
-            log.error("Expired JWT token, 만료된 JWT token 입니다.");
+            // 만료된 JWT 토큰
+            throw new GatewayException(EXPIRED_JWT_TOKEN, HttpStatus.UNAUTHORIZED);
         } catch (UnsupportedJwtException e) {
-            log.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
+            // 지원되지 않는 JWT 토큰
+            throw new GatewayException(UNSUPPORTED_JWT_TOKEN, HttpStatus.BAD_REQUEST);
         } catch (IllegalArgumentException e) {
-            log.error("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
+            // 잘못된 JWT 토큰
+            throw new GatewayException(INVALID_JWT_TOKEN, HttpStatus.BAD_REQUEST);
         }
-        return false;
     }
 }
