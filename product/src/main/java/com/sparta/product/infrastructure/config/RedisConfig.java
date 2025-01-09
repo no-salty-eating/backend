c6b5bc8dbd855f3381513c24c7fbed0918c8a7d7
HongChan Yoon <hongchani0324@gmail.com>
2025-01-10 02:21:29 +0900

    [feature] #55: 상품 재고 감소 API - controller에서만 Response 형태로 반환하도록 service return 형식 변경.

--- product/src/main/java/com/sparta/product/infrastructure/config/RedisConfig.java
+++ product/src/main/java/com/sparta/product/infrastructure/config/RedisConfig.java
@@ -1,9 +1,11 @@
 package com.sparta.product.infrastructure.config;
 
+import com.sparta.product.application.dtos.product.ProductResponseDto;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -16,6 +18,17 @@ public class RedisConfig {
         template.setValueSerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, ProductResponseDto> productResponseTemplate(
+            RedisConnectionFactory redisConnectionFactory
+    ) {
+        RedisTemplate<String, ProductResponseDto> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(RedisSerializer.string());
+        template.setValueSerializer(RedisSerializer.json());
         return template;
     }
 }
