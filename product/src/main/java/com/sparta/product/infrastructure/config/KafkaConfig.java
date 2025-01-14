7a82470a946b45f87da91fbcb6173852114d07ed
HongChan Yoon <hongchani0324@gmail.com>
2025-01-14 10:19:52 +0900

    [feature] #76: dockerfile, docker-compose 작성중.. (profile : dev) - mysql 정상적으로 작동 - mysql 헬스 체크 후 app 정상 실행 확인 - network 설정 시 다른 위치의 docker compose에서 만들어진 외부 network 사용하려면 external을 명시적으로 작성해야함.

--- product/src/main/java/com/sparta/product/infrastructure/config/KafkaConfig.java
+++ product/src/main/java/com/sparta/product/infrastructure/config/KafkaConfig.java
@@ -13,13 +13,14 @@ import org.springframework.kafka.core.*;
 import java.util.HashMap;
 import java.util.Map;
 
+// yml에서도 설정 가능.
 @Configuration
 public class KafkaConfig {
 
-    @Value("${host.url}")
+    @Value("${host.kafka.url}")
     private String hostUrl;
 
-    @Value("${kafka.port}")
+    @Value("${host.kafka.port}")
     private String kafkaPort;
 
     @Bean
@@ -36,20 +37,12 @@ public class KafkaConfig {
         return new KafkaTemplate<>(producerFactory());
     }
 
-    // Kafka 컨슈머 팩토리를 생성하는 빈을 정의합니다.
-    // ConsumerFactory는 Kafka 컨슈머 인스턴스를 생성하는 데 사용됩니다.
-    // 각 컨슈머는 이 팩토리를 통해 생성된 설정을 기반으로 작동합니다.
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
-        // 컨슈머 팩토리 설정을 위한 맵을 생성합니다.
         Map<String, Object> configProps = new HashMap<>();
-        // Kafka 브로커의 주소를 설정합니다.
         configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, hostUrl + ":" + kafkaPort);
-        // 메시지 키의 디시리얼라이저 클래스를 설정합니다.
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        // 메시지 값의 디시리얼라이저 클래스를 설정합니다.
         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        // 설정된 프로퍼티로 DefaultKafkaConsumerFactory를 생성하여 반환합니다.
         return new DefaultKafkaConsumerFactory<>(configProps);
     }
 
@@ -58,11 +51,9 @@ public class KafkaConfig {
     // 이 팩토리는 @KafkaListener 어노테이션이 붙은 메서드들을 실행할 컨테이너를 제공합니다.
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
-        // ConcurrentKafkaListenerContainerFactory를 생성합니다.
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        // 컨슈머 팩토리를 리스너 컨테이너 팩토리에 설정합니다.
+
         factory.setConsumerFactory(consumerFactory());
-        // 설정된 리스너 컨테이너 팩토리를 반환합니다.
         return factory;
     }
 }
