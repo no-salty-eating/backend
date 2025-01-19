0246096f12b0ea532f935540a2d8d85c5548686e
lje-ejl <zizz135777@gmail.com>
2025-01-20 02:04:39 +0900

    chore: redissonClient 코덱 설정

--- coupon/src/main/java/com/sparta/coupon/infrastructure/config/RedisConfig.java
+++ coupon/src/main/java/com/sparta/coupon/infrastructure/config/RedisConfig.java
@@ -2,6 +2,7 @@ package com.sparta.coupon.infrastructure.config;
 
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -19,8 +20,10 @@ public class RedisConfig {
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
+        config.setCodec(new StringCodec());
         config.useSingleServer()
-                .setAddress("redis://" + host + ":" + port+"/0");
+                .setAddress("redis://" + host + ":" + port + "/0");
         return Redisson.create(config);
     }
+
 }
